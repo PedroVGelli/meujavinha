9bd2d4bdf8ba89dedc18df855c6041094ffc7603
Leticia <leticialunu@gmail.com>
2024-07-01 17:39:58 -0300

    first commit

---
+++ exercicios.js
@@ -0,0 +1,103 @@
+//Questão 1
+let lacraio = 20
+
+console.log(lacraio)
+console.log("Hello World")
+
+//Questão 2
+
+var calabreso = ["calango", "Torta", "Banana", "Plasma"]
+console.log(calabreso)
+
+//Questão 3
+
+//console.log(frutas(4))
+
+//Questão 4
+
+let lado = 3;
+
+let area = lado ** 2
+console.log("A area de um quadrado de lado", lado, "é igual a", area, "m²")
+
+//Questão 5
+
+let a = 15
+let b = 20
+let c = 40
+let soma = b + a
+
+console.log(soma)
+if (soma < c) {
+    console.log("C é maior do que a soma")
+}
+
+//Questão 6
+
+//ERROR
+
+//Questão 7
+
+let num = 2
+let nun = 20
+
+somes = num + nun
+multiplics = num * nun
+
+
+if (num == nun) {
+    somes = num + nun
+}
+console.log(somes)
+
+if (num != nun) {
+    console.log(multiplics)
+}
+
+//Questão 8
+
+for (var contador = 0; contador < 5; contador++) {
+
+}
+console.log(contador)
+
+//Com "For" a lista aparece no console até o numero 5, caso troquemos para "let"
+// teremos um erro, pois a variavel "Contador" não foi previamente especificada.
+
+//Questão 9
+
+var numero;
+
+//numero = Number(window.prompt("Digite um numero"))
+//window.alert("Antercessor:" + String(numero - 1))
+//window.alert("Sucessor:" + String(numero + 1))
+
+//Questão 10
+
+var x = 10
+
+function Minhafunção() {
+    var y = 5;
+    console.log(y + x);
+}
+
+Minhafunção();
+//Não aparece nada na saida do console
+
+//Questão 11
+
+var numero = "5"
+
+if (numero === 5) {
+    console.log("O numero é igual a 5");
+} else if (numero == 5) {
+    console.log("O número é igual a 5, com a conversão de tipo.");
+
+} else {
+    console.log("O número não é igual a 5")
+}
+
+//A resposta é igual a opção do "else if" pois para o numero ficar "igual" é necessario uma conversão de tipo.
+
+//Questão 12
+
